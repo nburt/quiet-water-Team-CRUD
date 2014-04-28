2878ad3fb70b34b2a14c41ba564219d58b95f5bf
Nathanael Burt <nathanael.burt@gmail.com>
2014-04-28 15:45:56 -0600

    User sees form to add new pictures
    Signed-off-by: Paul Wenig <pwenig@gmail.com>

--- spec/features/pictures_spec.rb
+++ spec/features/pictures_spec.rb
@@ -2,10 +2,18 @@ require 'spec_helper'
 
 feature 'Picture manager' do
 
-  scenario 'User can click the all pictures link' do
+  scenario 'User can view pictures page and pictures form' do
     visit '/'
     click_on 'all pictures'
     expect(page).to have_content 'Pictures'
+
+    click_link 'New Picture'
+    expect(page).to have_content 'URL of Picture'
+    expect(page).to have_content 'Short Description'
+    expect(page).to have_content 'Rating'
+    find_field 'url'
+    find_field 'description'
+    find_field 'rating'
   end
 
 end
