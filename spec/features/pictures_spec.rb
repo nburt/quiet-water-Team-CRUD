c98964fa7f757181e1db99470a75a4c86435fe1f
Jenny Allar <jennyallar@gmail.com>
2014-04-29 16:36:16 -0600

    can view all pictures on homepage and click on pictures to go to individual show page
    Signed-off-by: Nathanael Burt <nathanael.burt@gmail.com>

--- spec/features/pictures_spec.rb
+++ spec/features/pictures_spec.rb
@@ -2,28 +2,47 @@ require 'spec_helper'
 
 feature 'Picture manager' do
 
-  scenario 'User can add pictures' do
-    visit '/'
-    click_on 'all pictures'
-    expect(page).to have_content 'Pictures'
-
-    click_link 'New Picture'
+  before do
+    visit '/pictures/new'
     expect(page).to have_content 'Add a picture'
-    expect(page).to have_content 'URL of Picture'
-    expect(page).to have_content 'Short Description'
-    expect(page).to have_content 'Rating'
+    fill_in 'url', with: 'http://i0006.photobucket.com/albums/0006/findstuff22/Best%20Images/Animals/6a81f2da.jpg'
+    fill_in 'description', with: 'Cool canyon photo'
+    fill_in 'rating', with: 5
+    click_on 'Create Picture'
+  end
+
+  scenario 'User can add pictures' do
+    expect(page).to have_content 'http://i0006.photobucket.com/albums/0006/findstuff22/Best%20Images/Animals/6a81f2da.jpg'
+    expect(page).to have_content 'Cool canyon photo'
+    expect(page).to have_content '5'
+    expect(page).to have_selector("img[src$='http://i0006.photobucket.com/albums/0006/findstuff22/Best%20Images/Animals/6a81f2da.jpg']")
+    expect(page).to have_content 'Picture successfully created'
+  end
+
+  scenario 'User sees error messages if they don\'t fill in the url or description fields' do
+    visit '/pictures'
+    click_link 'New Picture'
     fill_in 'rating', with: 5
     click_on 'Create Picture'
     expect(page).to have_content 'URL cannot be blank'
     expect(page).to have_content 'Description cannot be blank'
-    fill_in 'url', with: 'http://i0006.photobucket.com/albums/0006/findstuff22/Best%20Images/Animals/6a81f2da.jpg'
-    fill_in 'description', with: 'Cool canyon photo'
+  end
+
+  scenario 'User can see all pictures' do
+    visit '/pictures/new'
+    expect(page).to have_content 'Add a picture'
+    fill_in 'url', with: 'http://i1095.photobucket.com/albums/i469/azrie1/music/ItsGonnaBeMe.jpg'
+    fill_in 'description', with: "Cool 'NSync photo"
+    fill_in 'rating', with: 5
     click_on 'Create Picture'
-    expect(page).to have_content 'http://i0006.photobucket.com/albums/0006/findstuff22/Best%20Images/Animals/6a81f2da.jpg'
-    expect(page).to have_content 'Cool canyon photo'
-    expect(page).to have_content '5'
-    expect(page).should have_selector("img[src$='http://i0006.photobucket.com/albums/0006/findstuff22/Best%20Images/Animals/6a81f2da.jpg']")
-    expect(page).to have_content 'Picture successfully created'
+    id = current_path.match(/\d$/)
+    click_link 'all pictures'
+    expect(page).to have_selector("img[src$='http://i0006.photobucket.com/albums/0006/findstuff22/Best%20Images/Animals/6a81f2da.jpg']")
+    expect(page).to have_selector("img[src$='http://i1095.photobucket.com/albums/i469/azrie1/music/ItsGonnaBeMe.jpg']")
+    within '#images' do
+      page.first('a').click
+    end
+    expect(page).to have_content "Cool canyon photo"
   end
 
 end
