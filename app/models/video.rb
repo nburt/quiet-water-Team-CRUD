3f3a20f539eb6e0baf2a8fbbf938e0974f52d47f
Nathanael Burt <nathanael.burt@gmail.com>
2014-04-30 16:16:11 -0600

    user cannot enter invalid image or video links
    can only use .png, .gif, .jpg, .jpeg images
    
    can only use valid youtube links
    
    Signed-off-by: Jenny Allar <jennyallar@gmail.com>

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,10 @@ class Video < ActiveRecord::Base
   validates_presence_of :url, message: "URL cannot be blank"
   validates_presence_of :description, message: "Description cannot be blank"
   validates_length_of :description, maximum: 255, message: "Description cannot be longer than 255 characters"
-end 
+
+  validates :url,
+            format: {
+              with: /watch\?v=.{11}\Z/,
+              message: "URL must be valid"
+            }
+end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -2,32 +2,34 @@ require 'spec_helper'
 
 describe Video do
   describe "validations" do
+    before do
+      @video = Video.create!(url: "https://www.youtube.com/watch?v=VN8ze3S0Uj8", description: "top gun")
+
+    end
 
     it 'url cannot be empty' do
-      video = Video.create!(url: "https://www.youtube.com/watch?v=VN8ze3S0Uj8", description: "top gun")
+      @video.url = ""
 
-      video.url = ""
-
-      expect(video).to_not be_valid
-
+      expect(@video).to_not be_valid
     end
 
     it 'description is less than or equal to 255 characters' do
-      video = Video.create!(url: "https://www.youtube.com/watch?v=VN8ze3S0Uj8", description: "bottom gun")
-
-      video.description = "7.3 errors.add
+      @video.description = "7.3 errors.add
 The add method lets you manually add messages that are related to particular attributes. You can use the errors.full_messages or errors.to_a methods to view the messages in the form they might be displayed to a user. Those particular messages get the attribute name prepended (and capitalized). add receives the name of the attribute you want to add the message to, and the message itself."
 
-      expect(video).to_not be_valid
+      expect(@video).to_not be_valid
     end
 
     it 'description cannot be empty' do
-      video = Video.create!(url: "https://www.youtube.com/watch?v=VN8ze3S0Uj8", description: "middle gun")
+      @video.description = ""
 
-      video.description = ""
-
-      expect(video).to_not be_valid
+      expect(@video).to_not be_valid
     end
 
+    it 'must be a valid url' do
+      @video.url = "https://www.youtube.com/"
+
+      expect(@video).to_not be_valid
+    end
   end
 end
