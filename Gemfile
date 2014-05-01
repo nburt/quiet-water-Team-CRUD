06d2329ffb058bd3ec6055446e2c539ecdd548c2
Mike Kauffman <mike.a.kauffman@gmail.com>
2014-05-01 15:06:30 -0600

    Users can view the most recent 20 photos posted within 1km from specified lat long.

--- Gemfile
+++ Gemfile
@@ -1,13 +1,14 @@
 source 'https://rubygems.org'
 ruby '2.1.1'
 
+gem 'dotenv-rails', '~> 0.11.1'
 gem 'rails', '4.1.0'
 gem 'pg'
 gem 'sass-rails', '~> 4.0.3'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
-#gem 'turbolinks'
+gem 'instagram', '~> 1.0.0'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
--- app/controllers/instagrams_controller.rb
+++ app/controllers/instagrams_controller.rb
@@ -1,7 +1,19 @@
 class InstagramsController <ApplicationController
+  require "instagram"
+  require 'pp'
+
+  CALLBACK_URL = "http://localhost:4567/oauth/callback"
+
+  Instagram.configure do |config|
+    config.client_id = ENV['CLIENT_ID']
+    config.client_secret = ENV['CLIENT_SECRET']
+  end
 
   def index
+    @meta = 10
+    @html = Instagram.media_search("40.0176335","-105.2819911")
 
   end
+
 
 end
